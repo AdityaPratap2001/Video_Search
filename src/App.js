c32a0345a12dbfcafe7155d8953adac955217ebc
Aditya Pratap Singh <60472821+AdityaPratap2001@users.noreply.github.com>
2020-08-22 23:30:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './Components/SearchBar';
+import youtube from './apis/youtube';
+import VideoList from './Components/VideoList';
+import VideoDetail from './Components/VideoDetail';
+
+const KEY = 'AIzaSyCA869NL1F5AFiGiT8rPXACkVaJ-hSX1ZQ';
+
+class App extends Component {
+
+  state = { videos : [] , selectedVideo : null};
+
+  componentDidMount(){
+    this.onTermSubmit('s20 official video');
+  }
+
+  onTermSubmit = async term =>{
+    const response = await youtube.get('/search',{
+      params : {
+        part : 'snippet',
+        maxResults : 5,
+        key : KEY,
+        q : term
+      }
+    });
+    this.setState({
+      videos : response.data.items,
+      selectedVideo : response.data.items[0],
+    })
+  };
+
+  onVideoSelect = (video) =>{
+    this.setState({selectedVideo : video});
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{marginTop:'20px'}}>
+        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+            <div className="five wide column">
+              <VideoList 
+              onVideoSelect={this.onVideoSelect} 
+              videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  
+  state = {term : ''}
+
+  onInputChange = (event) =>{
+    this.setState({term : event.target.value});
+  };
+  onFormSubmit = event =>{
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment search-bar">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            
+            <label>Video Search</label>
+            <input type="text" value={this.state.term}
+                    onChange={this.onInputChange}
+            />
+
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
